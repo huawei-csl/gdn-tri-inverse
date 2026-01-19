304d6c21d0983b73fcc0ac7364960c1fe87b5591
Aleksandros Sobczyk <aleksandros.sobczyk@h-partners.com>
2026-01-19 13:48:48 +0000

    Add wrapper for matmul-based tri inverse

--- src/gdn_tri_inverse/linalg.py
+++ src/gdn_tri_inverse/linalg.py
@@ -1,10 +1,14 @@
 import torch
 from typing import Optional
-from tcuscan import run_tri_inv_col_sweep
+from tcuscan import run_tri_inv_col_sweep, run_triu_inv_rec_unroll
 
 def inv_tril_inplace(A: torch.Tensor):
-    """compute inv(I - A) where A is strict lower-triangular"""
-    # Adapted from https://github.com/huggingface/transformers/blob/v4.57.1/src/transformers/models/qwen3_next/modeling_qwen3_next.py#L485-L490
+    """
+    compute inv(I - A) where A is strict lower-triangular. Adapted from: 
+    https://github.com/huggingface/transformers/blob/v4.57.1/src/transformers/models/qwen3_next/modeling_qwen3_next.py#L485-L490
+    The algorithm is somewhat "in-place", in the sense that it does not 
+    explicitly form a full matrix for the inverse.
+    """
     assert A.shape[-2] == A.shape[-1]
     chunk_size = A.shape[-1]
     for i in range(1, chunk_size):
@@ -12,11 +16,37 @@ def inv_tril_inplace(A: torch.Tensor):
         sub = A[..., :i, :i].clone()
         A[..., i, :i] = row + (row.unsqueeze(-1) * sub).sum(-2)
 
+def tri_inv_qwen3_next_default(A: torch.Tensor):
+    """
+    This is a wrapper to the inv_tril_inplace defined above.
+    It is the default kernel used by qwen3 next.
+    We only define this function in order to have a uniform 
+    signature for all inversion kernels.
+    """
+    A_inv = A.clone()
+    inv_tril_inplace(A_inv)
+    A_inv = A_inv + torch.eye(A.shape[-1], dtype=A.dtype, device=A.device)
+    return A_inv
+
+
 def tri_inv_vcs(A: torch.Tensor) -> torch.Tensor:
-    A_view = A
-    if len(A.shape) == 4:
-        A_view = A.view(-1, *A.shape[2:])
+    """
+    VCS stands for Vector Column Sweep. The algorithm uses internally
+    only vector units (AIV).
+    """
+    n = A.shape[-1]
+    A_view = A.view(-1, n, n)
     A_inv = run_tri_inv_col_sweep(A_view)
     return A_inv.reshape(A.shape)
 
+def tri_inv_mxr(A: torch.Tensor) -> torch.Tensor:
+    """
+    The individual matrices of the tensor A must be strictly
+    upper triangular. The algorithm implements a matmul-based mixed 
+    recursive strategy (hence the MXR acronym) to compute the inverses.
+    """
+    n = A.shape[-1]
+    A_view = A.view(-1, n, n)
+    A_inv = run_triu_inv_rec_unroll(A_view)
+    return A_inv.reshape(A.shape)
 
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -7,62 +7,38 @@ import torch.nn.functional as torch_func
 import torch_npu
  
 from gdn_tri_inverse.linalg import (
-    inv_tril_inplace, tri_inv_vcs
+    tri_inv_qwen3_next_default, tri_inv_vcs, tri_inv_mxr
 )
  
 device = "npu:0"  # pick an available device
 
 def gen_random_matrix(shape, dtype):
     A = 0.1 * torch.abs(torch.rand(*shape, dtype=dtype))
-    A = torch.tril(A, diagonal=-1)
     return A
 
- 
-@pytest.mark.parametrize("chunk_size", [4, 8, 16, 32, 64])
+@pytest.mark.parametrize("chunk_size", [32, 64, 128])
 @pytest.mark.parametrize("batch", [1, 17])
 @pytest.mark.parametrize(
-    "dtype,atol,rtol", [(torch.float32, 1e-8, 5e-5), (torch.float16, 1e-5, 1e-2)]
+    "tri_inv_fn,dtype,atol,rtol", [
+    (tri_inv_mxr, torch.float16, 1e-5, 1e-2),
+    (tri_inv_vcs, torch.float16, 1e-5, 1e-2),
+    (tri_inv_vcs, torch.float32, 1e-8, 5e-5),
+    (tri_inv_qwen3_next_default, torch.float16, 1e-5, 1e-2),
+    (tri_inv_qwen3_next_default, torch.float32, 1e-8, 5e-5)
+    ]
 )
 @pytest.mark.parametrize('matrix_gen', [gen_random_matrix])
 @torch.inference_mode()
-def test_inv_tril_inplace(chunk_size, batch, dtype, atol, rtol, matrix_gen):
+def test_tri_inv(chunk_size, batch, tri_inv_fn, dtype, atol, rtol, matrix_gen):
     torch.manual_seed(0)
     shape = (batch, chunk_size, chunk_size)
  
     A = matrix_gen(shape=shape, dtype=dtype)
+    A = torch.tril(A, diagonal=-1)
 
     A_npu = A.to(device)
     torch.npu.synchronize()
-    A_inv_npu = A.clone()
-    torch.npu.synchronize()
-    inv_tril_inplace(A_inv_npu)
-    torch.npu.synchronize()
-    A_inv = A_inv_npu.cpu()
-    torch.npu.synchronize()
- 
-    ref_dtype = torch.float64
-    A_inv = A_inv.to(ref_dtype)
-    I = torch.eye(chunk_size, dtype=ref_dtype)
-    I_recover = (I - A.to(ref_dtype)) @ (I + A_inv)
-    assert_close(I_recover, I.expand_as(I_recover), atol=chunk_size*atol, rtol=chunk_size*rtol)
-
-
-@pytest.mark.parametrize("chunk_size", [16, 32, 64])
-@pytest.mark.parametrize("batch", [1, 17])
-@pytest.mark.parametrize(
-    "dtype,atol,rtol", [(torch.float32, 1e-8, 5e-5), (torch.float16, 1e-5, 1e-2)]
-)
-@pytest.mark.parametrize('matrix_gen', [gen_random_matrix])
-@torch.inference_mode()
-def test_tri_inv_vcs(chunk_size, batch, dtype, atol, rtol, matrix_gen):
-    torch.manual_seed(0)
-    shape = (batch, chunk_size, chunk_size)
- 
-    A = matrix_gen(shape=shape, dtype=dtype)
-
-    A_npu = A.to(device)
-    torch.npu.synchronize()
-    A_inv_npu = tri_inv_vcs(A_npu)
+    A_inv_npu = tri_inv_fn(A_npu)
     torch.npu.synchronize()
     A_inv = A_inv_npu.cpu()
     torch.npu.synchronize()
@@ -72,4 +48,3 @@ def test_tri_inv_vcs(chunk_size, batch, dtype, atol, rtol, matrix_gen):
     I = torch.eye(chunk_size, dtype=ref_dtype)
     I_recover = (I + A.to(ref_dtype)) @ A_inv
     assert_close(I_recover, I.expand_as(I_recover), atol=chunk_size * atol, rtol=chunk_size * rtol)
- 
