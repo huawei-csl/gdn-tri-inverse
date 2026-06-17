ee5a5fd1f67ede9399cfb16adc1260b1db3d5921
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-06-17 11:49:55 +0200

    Adapt scripts after re-organization

--- Makefile
+++ Makefile
@@ -7,9 +7,9 @@ test_tri_inv:
 	python -m pytest -v tests/test_linalg.py
 
 profile_tri_inv:
-	bash profiling/run_profiling_tri_inv.sh
+	bash profiling/kernel_performance/run_profiling_tri_inv.sh
 	pip install jupyter ipykernel nbconvert pandas seaborn
-	export PATH=${PATH}:${HOME}/.local/bin/ && jupyter nbconvert --to notebook --inplace --execute profiling/nbs/plots_tri_inv.ipynb
+	export PATH=${PATH}:${HOME}/.local/bin/ && jupyter nbconvert --to notebook --inplace --execute profiling/kernel_performance/plots_tri_inv.ipynb
 
 install:
 	export CMAKE_GENERATOR="Unix Makefiles" && . /usr/local/Ascend/ascend-toolkit/set_env.sh && pip install -v . --extra-index-url https://download.pytorch.org/whl/cpu
--- profiling/kernel_performance/run_profiling_triton_gdn.sh
+++ profiling/kernel_performance/run_profiling_triton_gdn.sh
@@ -1,7 +1,8 @@
 export CMAKE_GENERATOR="Unix Makefiles"
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
-
-python profiling/profile_triton_gdn_npu.py --chunk-size 64 --inverse-type column-sweep
-python profiling/profile_triton_gdn_npu.py --chunk-size 64 --inverse-type cube-rec-unroll
-python profiling/profile_triton_gdn_npu.py --chunk-size 64 --inverse-type bsnd-rec-unroll
-python profiling/profile_triton_gdn_npu.py --chunk-size 64 --inverse-type triton
+PYTHON_SCRIPT="$(dirname "$(realpath "$0")")"/profile_triton_gdn_npu.py
+DATA_PATH="$(dirname "$(realpath "$0")")/../data/Qwen3-Next"
+python ${PYTHON_SCRIPT} --chunk-size 64 --input ${DATA_PATH} --inverse-type column-sweep
+python ${PYTHON_SCRIPT} --chunk-size 64 --input ${DATA_PATH} --inverse-type cube-rec-unroll
+python ${PYTHON_SCRIPT} --chunk-size 64 --input ${DATA_PATH} --inverse-type bsnd-rec-unroll
+python ${PYTHON_SCRIPT} --chunk-size 64 --input ${DATA_PATH} --inverse-type triton
