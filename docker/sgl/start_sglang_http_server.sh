d334d030f68f01185962747e890aa5f89f0453a2
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-06-25 16:07:02 +0200

    Comment out visible devices

--- docker/sgl/start_sglang_http_server.sh
+++ docker/sgl/start_sglang_http_server.sh
@@ -14,7 +14,7 @@ source /usr/local/Ascend/nnal/atb/set_env.sh
 
 export MODEL_NAME="${MODEL_NAME:-Qwen/Qwen3.5-0.8B-Base}"
 export ATTENTION_BACKEND="${ATTENTION_BACKEND:-ascend}"
-export ASCEND_RT_VISIBLE_DEVICES=1,2 # Comma-separated device ids
+# export ASCEND_RT_VISIBLE_DEVICES=1,2 # Comma-separated device ids
 
 echo "[GDN-TRI-INVERSE] Starting SGLANG server."
 echo "[GDN-TRI-INVERSE] Press Ctrl+z and 'bg' to send process in background"
