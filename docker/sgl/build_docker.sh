0aa5933537a10a554eaea49377ebb860d809a072
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-06-25 16:29:09 +0200

    Updating versions for sgl and torch

--- docker/sgl/build_docker.sh
+++ docker/sgl/build_docker.sh
@@ -3,14 +3,14 @@
 export GDN_TRI_INVERSE_LOCAL_PATH="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/"
 export GDN_TRI_INVERSE_COMMIT="${GDN_TRI_INVERSE_COMMIT:-"$(git rev-parse --verify HEAD)"}"
 export SGL_DOCKER_HOSTNAME="${SGL_DOCKER_HOSTNAME:-}"
-export SGL_KERNEL_NPU_BRANCH="${SGL_KERNEL_NPU_BRANCH:-6-triinv-integrate-tri_inv_cube_col_sweep-kernel}"
-export SGL_KERNEL_NPU_HTTPS_GIT_URL="${SGL_KERNEL_NPU_HTTPS_GIT_URL:-https://github.com/gioelegott/sgl-kernel-npu.git}"
+export SGL_KERNEL_NPU_BRANCH_OR_TAG="${SGL_KERNEL_NPU_BRANCH_OR_TAG:-2026.06.03}" # 6-triinv-integrate-tri_inv_cube_col_sweep-kernel
+export SGL_KERNEL_NPU_HTTPS_GIT_URL="${SGL_KERNEL_NPU_HTTPS_GIT_URL:-https://github.com/sgl-project/sgl-kernel-npu.git}" # https://github.com/gioelegott/sgl-kernel-npu.git
 export TILELANG_ASCEND_COMMIT="${TILELANG_ASCEND_COMMIT:-25f3eec9b15705c2bf46a28a882fc9a73f698f91}"
-export DOCKER_IMAGE_TAG="gdn-tri-inverse:8.5.0-25062026"
+export DOCKER_IMAGE_TAG="gdn-tri-inverse:9.0.0-25062026"
 
 pushd $GDN_TRI_INVERSE_LOCAL_PATH \
 && docker build --no-cache --build-arg SGL_DOCKER_HOSTNAME="${SGL_DOCKER_HOSTNAME}" \
-    --build-arg SGL_KERNEL_NPU_BRANCH="${SGL_KERNEL_NPU_BRANCH}" \
+    --build-arg SGL_KERNEL_NPU_BRANCH_OR_TAG="${SGL_KERNEL_NPU_BRANCH_OR_TAG}" \
     --build-arg SGL_KERNEL_NPU_HTTPS_GIT_URL="${SGL_KERNEL_NPU_HTTPS_GIT_URL}" \
     --build-arg TILELANG_ASCEND_COMMIT="${TILELANG_ASCEND_COMMIT}" \
     --build-arg GDN_TRI_INVERSE_COMMIT="${GDN_TRI_INVERSE_COMMIT}" \
