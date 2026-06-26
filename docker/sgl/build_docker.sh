bc5aa83f5540e671f7e9034d0eff42d0b8da7dcf
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-06-26 10:50:35 +0200

    Test without TileLang

--- docker/sgl/build_docker.sh
+++ docker/sgl/build_docker.sh
@@ -3,7 +3,7 @@
 export GDN_TRI_INVERSE_LOCAL_PATH="$(dirname $(readlink -f ${BASH_SOURCE[0]}))/"
 export GDN_TRI_INVERSE_COMMIT="${GDN_TRI_INVERSE_COMMIT:-"$(git rev-parse --verify HEAD)"}"
 export SGL_DOCKER_HOSTNAME="${SGL_DOCKER_HOSTNAME:-}"
-export SGL_KERNEL_NPU_BRANCH_OR_TAG="${SGL_KERNEL_NPU_BRANCH_OR_TAG:-2026.06.03}" # 6-triinv-integrate-tri_inv_cube_col_sweep-kernel
+export SGL_KERNEL_NPU_BRANCH_OR_TAG="${SGL_KERNEL_NPU_BRANCH_OR_TAG:-2026.6.2}" # 6-triinv-integrate-tri_inv_cube_col_sweep-kernel
 export SGL_KERNEL_NPU_HTTPS_GIT_URL="${SGL_KERNEL_NPU_HTTPS_GIT_URL:-https://github.com/sgl-project/sgl-kernel-npu.git}" # https://github.com/gioelegott/sgl-kernel-npu.git
 export TILELANG_ASCEND_COMMIT="${TILELANG_ASCEND_COMMIT:-25f3eec9b15705c2bf46a28a882fc9a73f698f91}"
 export DOCKER_IMAGE_TAG="gdn-tri-inverse:9.0.0-25062026"
@@ -12,7 +12,7 @@ pushd $GDN_TRI_INVERSE_LOCAL_PATH \
 && docker build --no-cache --build-arg SGL_DOCKER_HOSTNAME="${SGL_DOCKER_HOSTNAME}" \
     --build-arg SGL_KERNEL_NPU_BRANCH_OR_TAG="${SGL_KERNEL_NPU_BRANCH_OR_TAG}" \
     --build-arg SGL_KERNEL_NPU_HTTPS_GIT_URL="${SGL_KERNEL_NPU_HTTPS_GIT_URL}" \
-    --build-arg TILELANG_ASCEND_COMMIT="${TILELANG_ASCEND_COMMIT}" \
+    # --build-arg TILELANG_ASCEND_COMMIT="${TILELANG_ASCEND_COMMIT}" \
     --build-arg GDN_TRI_INVERSE_COMMIT="${GDN_TRI_INVERSE_COMMIT}" \
     -t "${DOCKER_IMAGE_TAG}" \
     -f Dockerfile . \
