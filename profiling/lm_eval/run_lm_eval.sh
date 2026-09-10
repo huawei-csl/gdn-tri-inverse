23b4970eb983f6b58fcfee7a34d422b2ff329925
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-09-10 10:10:50 +0200

    Update simple_query and run_lm_eval

--- profiling/lm_eval/run_lm_eval.sh
+++ profiling/lm_eval/run_lm_eval.sh
@@ -1,8 +1,14 @@
 export BASE_URL="http://localhost:30000/v1/completions,tokenized_requests=False,num_concurrent=1"
-export MODEL_NAME="${MODEL_NAME:-Qwen/Qwen3.5-4B-Base}"
-export LM_EVAL_TASK="wikitext"
-lm_eval --model local-completions \
-    --tasks ${LM_EVAL_TASK} \
-    --model_args model=${MODEL_NAME},base_url=${BASE_URL} \
-    --batch_size auto
-
+export OUTPUT_PATH="${OUTPUT_PATH:-./results}"
+export MODEL_NAME="${MODEL_NAME:-Qwen/Qwen3.5-9B-Base}"
+export LM_EVAL_TASKS="longbench_single" # comma-separated tasks, e.g. 
+# wikitext,mmlu_anatomy,mmlu_abstract_algebra,mmlu_college_computer_science,mmlu_college_mathematics
+export NUM_ITERS=1
+for i in $(seq 1 $NUM_ITERS)
+do
+    time lm_eval --model local-completions \
+        --tasks ${LM_EVAL_TASKS} \
+        --model_args model=${MODEL_NAME},base_url=${BASE_URL} \
+        --batch_size auto \
+        --output_path ${OUTPUT_PATH} > /dev/null 2>&1
+done
