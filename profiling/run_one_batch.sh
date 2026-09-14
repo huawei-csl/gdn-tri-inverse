f7e1ac809e6729f317b52ea37b5c91255febfe4b
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-09-14 13:49:58 +0200

    Use --result-filename instead of mv result.jsonl ...

--- profiling/run_one_batch.sh
+++ profiling/run_one_batch.sh
@@ -1,12 +1,11 @@
 export ATTENTION_BACKEND="${ATTENTION_BACKEND:-ascend}"
-export ASCEND_RT_VISIBLE_DEVICES=3 # Comma-separated device ids
 export BATCH_SIZE=8
 export INPUT_LEN=4096
 export OUTPUT_LEN=32
 export N_ITERS=3
 export MODEL_NAME="Qwen/Qwen3.6-35B-A3B"
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3
-export NUM_NPUS=2
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3 # Comma-separated device ids
+export NUM_NPUS=2 # number of devices to use, corresponts to the --tp-size parameter
 
 export SOLVE_TRIL_BACKEND="default"
 export GDN_USE_MEGA_GDN=0
@@ -26,9 +25,9 @@ python3 -m sglang.bench_one_batch \
     --mem-fraction-static 0.7 \
     --batch-size ${BATCH_SIZE} \
     --input-len ${INPUT_LEN} \
-    --output-len ${OUTPUT_LEN}
+    --output-len ${OUTPUT_LEN} \
+    --result-filename "result_${BATCH_SIZE}_${INPUT_LEN}_default.jsonl"
 done
-mv result.jsonl result_${BATCH_SIZE}_${INPUT_LEN}_default.jsonl
 
 
 export SOLVE_TRIL_BACKEND="pto-mxr"
@@ -49,9 +48,9 @@ python3 -m sglang.bench_one_batch \
     --mem-fraction-static 0.7 \
     --batch-size ${BATCH_SIZE} \
     --input-len ${INPUT_LEN} \
-    --output-len ${OUTPUT_LEN}
+    --output-len ${OUTPUT_LEN} \
+    --result-filename "result_${BATCH_SIZE}_${INPUT_LEN}_pto.jsonl"
 done
-mv result.jsonl result_${BATCH_SIZE}_${INPUT_LEN}_pto.jsonl
 
 
 export SOLVE_TRIL_BACKEND="pto-mxr"
@@ -72,6 +71,6 @@ python3 -m sglang.bench_one_batch \
     --mem-fraction-static 0.7 \
     --batch-size ${BATCH_SIZE} \
     --input-len ${INPUT_LEN} \
-    --output-len ${OUTPUT_LEN}
+    --output-len ${OUTPUT_LEN} \
+    --result-filename "result.jsonl result_${BATCH_SIZE}_${INPUT_LEN}_mega.jsonl"
 done
-mv result.jsonl result_${BATCH_SIZE}_${INPUT_LEN}_mega.jsonl
